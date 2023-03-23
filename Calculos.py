80285d8eeb475cd85dc8f69fb3dbb06c74c403fd
Lorena <lorenafersilva17@gmail.com>
2023-03-23 11:12:29 -0300

    Feito as funções dos calculos de conversão, todos do sistema de multiplicação

---
+++ Calculos.py
@@ -0,0 +1,118 @@
+def converterStringParaFloat(value):
+    print('Valor convertido de str para float')
+    return float(value)
+
+def bitParaByte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    bytesCalculado = valorASerConvertido / 8
+    return bytesCalculado
+
+def byteParaBit(valorASerConvertido):
+    print('Valor convertido de byte para bit')
+    bitsCalculado = valorASerConvertido * 8
+    return bitsCalculado
+
+print('Insira o valor a ser convertido')
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido = byteParaBit(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
+
+
+def converterStringParaFloat(value):
+    print('Valor')
+    return float(value)
+
+def bitParaByte(valorASerConvertido):
+    print('Valor convertido de byte para kilobyte')
+    kilobytesCalculado = valorASerConvertido / 1024
+    return kilobytesCalculado
+
+def kilobyteParaByte(valorASerConvertido):
+    print('Valor convertido de kilobyte para byte')
+    bitsCalculado = valorASerConvertido * 1024
+    return bitsCalculado
+
+print('Insira o valor a ser convertido')
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido = kilobyteParaByte(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
+
+
+
+def converterStringParaFloat(value):
+    print('Valor')
+    return float(value)
+
+def bitParaByte(valorASerConvertido):
+    print('Valor convertido de megabyte para kilobyte')
+    kilobytesCalculado = valorASerConvertido / 1024
+    return kilobytesCalculado
+
+def megabyteParakiloByte(valorASerConvertido):
+    print('Valor convertido de megabyte para kilobyte')
+    bitsCalculado = valorASerConvertido * 1024
+    return bitsCalculado
+
+print('Insira o valor a ser convertido')
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido = megabyteParakiloByte(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
+
+
+def converterStringParaFloat(value):
+    print('Valor')
+    return float(value)
+
+def bitParaByte(valorASerConvertido):
+    print('Valor convertido de byte para kilobyte')
+    kilobytesCalculado = valorASerConvertido / 1024
+    return kilobytesCalculado
+
+def gigabyteParamegaByte(valorASerConvertido):
+    print('Valor convertido de gigabyte para megabyte')
+    bitsCalculado = valorASerConvertido * 1024
+    return bitsCalculado
+
+print('Insira o valor a ser convertido')
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido = gigabyteParamegaByte(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
+
+
+def converterStringParaFloat(value):
+    print('Valor')
+    return float(value)
+
+def bitParaByte(valorASerConvertido):
+    print('Valor convertido de byte para kilobyte')
+    kilobytesCalculado = valorASerConvertido / 1024
+    return kilobytesCalculado
+
+def terabyteParagigaByte(valorASerConvertido):
+    print('Valor convertido de terabyte para gigabyte')
+    bitsCalculado = valorASerConvertido * 1024
+    return bitsCalculado
+
+print('Insira o valor a ser convertido')
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido = terabyteParagigaByte(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
+
+def converterStringParaFloat(value):
+    print('Valor')
+    return float(value)
+
+def bitParaByte(valorASerConvertido):
+    print('Valor convertido de byte para kilobyte')
+    kilobytesCalculado = valorASerConvertido / 1024
+    return kilobytesCalculado
+
+def petabyteParateraByte(valorASerConvertido):
+    print('Valor convertido de petabyte para terabyte')
+    bitsCalculado = valorASerConvertido * 1024
+    return bitsCalculado
+
+print('Insira o valor a ser convertido')
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido = petabyteParateraByte(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
